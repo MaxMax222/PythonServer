080cc24a92fa8f2f0dcc2d9ece48da65d06c8e09
Maxim Zyuryaev <zrv.maxim@gmail.com>
2024-06-18 19:48:09 +0300

    made the code modular

--- client.py
+++ client.py
@@ -9,33 +9,40 @@ client.connect(('localhost', port))
 
 finished = False
 
+def send_files(client):
+    done = False
+    while not done:
+        file_path = input('Enter path to a file that you want to upload: -> ')
+        if os.path.exists(file_path):
+            with open(file_path, 'rb') as file:
+                client.send(file.name.encode())
+                client.sendfile(file)
+            done = input('Do you wish to upload any other files? [y/n]: -> ') == 'n'
+        else:
+            print("File doesn't exist. Please enter a valid file path.")
+
+def download_files(client):
+    done = False
+    while not done:
+        file_name = input('Enter file name that you want to download: -> ')
+        client.send(file_name.encode())
+        data = client.recv(2048)
+        if data:
+            with open(file_name, 'wb') as file:
+                file.write(data)
+                os.rename(file.name, r'c:\Users\97254\Downloads')
+            print(f"{file_name} downloaded successfully.")
+        else:
+            print("File not found on the server.")
+        done = input('Do you wish to download any other files? [y/n]: -> ') == 'n'
+
 while not finished:
     operation = int(input('Would you like to upload or download a file from the server? [1 - upload / 2 - download] -> '))
     client.send(str(operation).encode())
     if operation == 1:
-        done = False
-        while not done:
-            file_path = input('Enter path to a file that you want to upload: -> ')
-            if os.path.exists(file_path):
-                with open(file_path, 'rb') as file:
-                    client.send(file.name.encode())
-                    client.sendfile(file)
-                done = input('Do you wish to upload any other files? [y/n]: -> ') == 'n'
-            else:
-                print("File doesn't exist. Please enter a valid file path.")
+        send_files(client)
     else:
-        done = False
-        while not done:
-            file_name = input('Enter file name that you want to download: -> ')
-            client.send(file_name.encode())
-            data = client.recv(2048)
-            if data:
-                with open(file_name, 'wb') as file:
-                    file.write(data)
-                    os.rename(file.name, r'c:\Users\97254\Downloads')
-                print(f"{file_name} downloaded successfully.")
-            else:
-                print("File not found on the server.")
-            done = input('Do you wish to download any other files? [y/n]: -> ') == 'n'
+        download_files(client)
     finished = input('Do you wish to finish? [y/n]: -> ') == 'y'
+    
 client.close()
--- server.py
+++ server.py
@@ -10,28 +10,33 @@ print(f'Socket bound to port {port}')
 server.listen(10)
 print('Socket listening')
 
+def recieve_file(client):
+    file_name = client.recv(2048).decode()  
+    file_path = os.path.join(r'c:\Users\97254\Desktop\PythonServerData', file_name)
+
+    with open(file_path, 'wb') as file:
+        while True:
+            data = client.recv(1024)
+            if not data:
+                break
+            file.write(data)
+
+def send_file(client):
+    file_name = client.recv(2048).decode()
+    if os.path.exists(file_name):
+        with open(file_name, 'rb') as file:
+            data = file.read()
+            client.send(data)
+    else:
+        client.send(b'File not found on server.')
+
 while True:
     client, addr = server.accept()
 
     operation = client.recv(2048).decode()
     if int(operation) == 1:
-
-        file_name = client.recv(2048).decode()  
-        file_path = os.path.join(r'c:\Users\97254\Desktop\PythonServerData', file_name)
-
-        with open(file_path, 'wb') as file:
-            while True:
-                data = client.recv(1024)
-                if not data:
-                    break
-                file.write(data)
+        recieve_file(client)
     else:
-        file_name = client.recv(2048).decode()
-        if os.path.exists(file_name):
-            with open(file_name, 'rb') as file:
-                data = file.read()
-                client.send(data)
-        else:
-            client.send(b'File not found on server.')
+        send_file(client)
 
     client.close()
